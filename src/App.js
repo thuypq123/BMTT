cfccd66653ea202bdd6d18e744e93adf32abcd2a
thuypq123 <lmthuy37@gmail.com>
2022-10-07 03:25:41 +0700

    final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Facebook from './components/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Facebook Auth Example</h1>
+      </header>
+      <p className="App-intro">To get Started, Authenticate with Facebook.</p>
+      <Facebook />
+    </div>
+  );
+}
+
+export default App;
